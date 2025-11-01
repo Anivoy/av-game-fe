3e1bd14d1ee8bc8d56a7a4cda4e2b9cfe1e4ba6e
Fadil Hisyam <ichorvfx@gmail.com>
2025-11-01 21:09:20 +0700

    feat: project initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="font-bold text-xl">
+        Welcome to AnimeScape Frontend (Game) Project Scaffold
+      </h1>
+    </div>
+  )
+}
+
+export default App
